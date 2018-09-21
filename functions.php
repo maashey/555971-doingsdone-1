f9a8c5495d05011914affafea7326ce95d9860f4
maashey <mbalueva@gmail.com>
2018-09-21 17:58:08 +0300

    добавлена функция шаблонизатор и шаблоны index.php и layout.php, внесены изменения в index.php

---
+++ functions.php
@@ -0,0 +1,33 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+//функция подсчёта кол-ва задач в проекте
+function task_count($tasks, $project){
+  $count=0;
+  foreach ($tasks as $task) {
+    if ($task['Категория'] == $project ){
+      $count++;
+    }
+  }
+  return $count;
+}
+
+//функция шаблонизатор
+function include_template($name, $data) {
+    $name = 'templates/' . $name;
+    $result = '';
+
+    //Проверяем, что файл шаблона, переданный в аргументе, существует. Если не существует, то функция вернет пустую строку
+    if (!file_exists($name)) {
+        return $result;
+    }
+
+    ob_start(); //включение буферизации вывода
+    extract($data); // extract делает из массива набор переменных в локальной области видимости
+    require($name); //переменные из extract будут видны внутри подключаемого файла
+
+    $result = ob_get_clean();  //возвращаем буфер и очищаем
+
+    return $result;
+}
